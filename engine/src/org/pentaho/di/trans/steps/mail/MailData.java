3c4e68da9219944e78262d87dc220f5e871c5de0
fskorgen <frank@skorgen.no>
2019-10-29 22:49:09 +0100

    [MRA10-209] ETL Mail - Add support for external mail configuration

--- engine/src/org/pentaho/di/trans/steps/mail/MailData.java
+++ engine/src/org/pentaho/di/trans/steps/mail/MailData.java
@@ -22,8 +22,8 @@
 
 package org.pentaho.di.trans.steps.mail;
 
-import java.util.HashSet;
 import java.util.Properties;
+import java.util.Set;
 
 import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMultipart;
@@ -39,79 +39,88 @@ import org.pentaho.di.trans.step.StepDataInterface;
  * @since 28-07-2008
  */
 public class MailData extends BaseStepData implements StepDataInterface {
-  public int indexOfDestination;
-  public int indexOfDestinationCc;
-  public int indexOfDestinationBCc;
+    public int indexOfDestination;
+    public int indexOfDestinationCc;
+    public int indexOfDestinationBCc;
 
-  public int indexOfSenderName;
-  public int indexOfSenderAddress;
+    public int indexOfSenderName;
+    public int indexOfSenderAddress;
 
-  public int indexOfContactPerson;
-  public int indexOfContactPhone;
+    public int indexOfContactPerson;
+    public int indexOfContactPhone;
 
-  public int indexOfServer;
-  public int indexOfPort;
+    public int indexOfServer;
+    public int indexOfPort;
 
-  public int indexOfAuthenticationUser;
-  public int indexOfAuthenticationPass;
+    public int indexOfAuthenticationUser;
+    public int indexOfAuthenticationPass;
 
-  public int indexOfSubject;
-  public int indexOfComment;
+    public int indexOfSubject;
+    public int indexOfComment;
 
-  public int indexOfSourceFilename;
-  public int indexOfSourceWildcard;
-  public long zipFileLimit;
+    public int indexOfSourceFilename;
+    public int indexOfSourceWildcard;
+    public long zipFileLimit;
 
-  public int indexOfDynamicZipFilename;
+    public int indexOfDynamicZipFilename;
 
-  public String ZipFilename;
+    public String ZipFilename;
 
-  Properties props;
+    Properties props;
 
-  public MimeMultipart parts;
+    public MimeMultipart parts;
 
-  public RowMetaInterface previousRowMeta;
+    public RowMetaInterface previousRowMeta;
 
-  public int indexOfReplyToAddresses;
+    public int indexOfReplyToAddresses;
 
-  public String realSourceFileFoldername;
+    public String realSourceFileFoldername;
 
-  public String realSourceWildcard;
+    public String realSourceWildcard;
 
-  public int nrEmbeddedImages;
-  public int nrattachedFiles;
+    public int nrEmbeddedImages;
+    public int nrattachedFiles;
 
-  public HashSet<MimeBodyPart> embeddedMimePart;
+    public Set<MimeBodyPart> embeddedMimePart;
 
-  public int indexOfAttachedContent;
-  public int IndexOfAttachedFilename;
+    public int indexOfAttachedContent;
+    public int IndexOfAttachedFilename;
 
-  public MailData() {
-    super();
-    indexOfDestination = -1;
-    indexOfDestinationCc = -1;
-    indexOfDestinationBCc = -1;
-    indexOfSenderName = -1;
-    indexOfSenderAddress = -1;
-    indexOfContactPerson = -1;
-    indexOfContactPhone = -1;
-    indexOfServer = -1;
-    indexOfPort = -1;
-    indexOfAuthenticationUser = -1;
-    indexOfAuthenticationPass = -1;
-    indexOfSubject = -1;
-    indexOfComment = -1;
-    indexOfSourceFilename = -1;
-    indexOfSourceWildcard = -1;
-    zipFileLimit = 0;
-    indexOfDynamicZipFilename = -1;
-    props = new Properties();
-    indexOfReplyToAddresses = -1;
-    embeddedMimePart = null;
-    nrEmbeddedImages = 0;
-    nrattachedFiles = 0;
-    indexOfAttachedContent = -1;
-    IndexOfAttachedFilename = -1;
-  }
+    // SKOFRA
+    boolean usingAuthentication = false;
+    boolean usingConfig = false;
+    String server = "";
+    int port = -1;
+    String protocol = "smtp";
+    String authenticationUser = "";
+    String authenticationPassword = "";
+
+    public MailData() {
+        super();
+        indexOfDestination = -1;
+        indexOfDestinationCc = -1;
+        indexOfDestinationBCc = -1;
+        indexOfSenderName = -1;
+        indexOfSenderAddress = -1;
+        indexOfContactPerson = -1;
+        indexOfContactPhone = -1;
+        indexOfServer = -1;
+        indexOfPort = -1;
+        indexOfAuthenticationUser = -1;
+        indexOfAuthenticationPass = -1;
+        indexOfSubject = -1;
+        indexOfComment = -1;
+        indexOfSourceFilename = -1;
+        indexOfSourceWildcard = -1;
+        zipFileLimit = 0;
+        indexOfDynamicZipFilename = -1;
+        props = new Properties();
+        indexOfReplyToAddresses = -1;
+        embeddedMimePart = null;
+        nrEmbeddedImages = 0;
+        nrattachedFiles = 0;
+        indexOfAttachedContent = -1;
+        IndexOfAttachedFilename = -1;
+    }
 
 }
