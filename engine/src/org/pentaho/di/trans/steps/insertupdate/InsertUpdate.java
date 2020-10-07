a6d1270af1b427a59689deadf662528b8bd393a8
fskorgen <frank@skorgen.no>
2020-10-07 10:52:54 +0200

    [DEEMETL-68] Add support for AUTO_CREATE_TABLES for StagingUpsert and UpdateInsert step

--- engine/src/org/pentaho/di/trans/steps/insertupdate/InsertUpdate.java
+++ engine/src/org/pentaho/di/trans/steps/insertupdate/InsertUpdate.java
@@ -28,6 +28,7 @@ import java.util.List;
 
 import org.apache.commons.lang.ArrayUtils;
 import org.pentaho.di.core.Const;
+import org.pentaho.di.core.SQLStatement;
 import org.pentaho.di.core.database.Database;
 import org.pentaho.di.core.database.DatabaseMeta;
 import org.pentaho.di.core.exception.KettleDatabaseException;
@@ -480,6 +481,20 @@ public class InsertUpdate extends BaseStep implements StepInterface {
           data.db.connect( getPartitionID() );
         }
         data.db.setCommit(  meta.getCommitSize( this ) );
+
+        // SKOFRA artf48614 : Auto create tables when running job
+        String schemaTable = data.db.getDatabaseMeta().getQuotedSchemaTableCombination(meta.getSchemaName(), meta.getTableName());
+        String autoCreateTables = getVariable("BI_AUTO_CREATE_TABLES", "NO");
+        if (autoCreateTables.startsWith("Y") && !data.db.checkTableExists(environmentSubstitute(schemaTable)) ) {
+            RowMetaInterface prev = getTransMeta().getPrevStepFields(getStepMeta().getName());
+            SQLStatement sql =  meta.getSQLStatements(getTransMeta(), getStepMeta() , prev, repository, metaStore); 
+            if (!sql.hasError()) {
+                if (sql.hasSQL()) {
+                    data.db.execStatements(sql.getSQL());
+                }
+            }
+        }
+        // SKOFRA artf48614 : Auto create tables when running job
 
         return true;
       } catch ( KettleException ke ) {
